f961553a64faed2e563fb782cdf5808c10bd5f2c
asier-ochoa <78745593+asier-ochoa@users.noreply.github.com>
2024-06-08 19:29:34 -0400

    Basic ncurses drawing

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,97 @@
 const std = @import("std");
+const curses = @cImport({
+    // @cDefine("NCURSES_WIDECHAR", "1");
+    @cInclude("curses.h");
+});
+const signal = @cImport(@cInclude("signal.h"));
+const locale = @cImport(@cInclude("locale.h"));
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const CursesError = error{
+    Return,
+};
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+fn cchar_tOf(comptime symbol: comptime_int) CursesError!curses.cchar_t {
+    const static = struct {
+        const s = symbol;
+        var cchar_t: ?curses.cchar_t = null;
+    };
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    // According to man setcchar, symbol must be null terminated
+    const term_symbol = [_]c_int{symbol, 0};
+    if (static.cchar_t) |_| {
+        return static.cchar_t.?;
+    } else {
+        static.cchar_t = .{};
+        switch (curses.setcchar(
+            &(static.cchar_t.?),
+            &term_symbol,
+            0, 0, null
+        )) {
+            curses.ERR => return CursesError.Return,
+            else => {}
+        }
+        return static.cchar_t.?;
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+const drawing = struct {
+    const v_line = '│';
+    const h_line = '─';
+    const tl_corner = '┌';
+    const tr_corner = '┐';
+    const bl_corner = '└';
+    const br_corner = '┘';
+};
+
+inline fn between(a: anytype, val: anytype, b: anytype) bool {
+    return a >= val and b <= val;
+}
+
+pub fn drawBox(x: i32, y: i32, width: u32, height: u32) !void{
+    const iheight: i32 = @intCast(height);
+    const iwidth: i32 = @intCast(width);
+    _ = curses.mvhline_set(y, x + 1, &(try cchar_tOf(drawing.h_line)), iwidth - 2);
+    _ = curses.mvhline_set(y + iheight - 1, x + 1, &(try cchar_tOf(drawing.h_line)), iwidth - 2);
+    _ = curses.mvvline_set(y + 1, x, &(try cchar_tOf(drawing.v_line)), iheight - 2);
+    _ = curses.mvvline_set(y + 1, x + iwidth - 1, &(try cchar_tOf(drawing.v_line)), iheight - 2);
+
+    // Corners
+    _ = curses.mvadd_wch(y, x, &(try cchar_tOf(drawing.tl_corner)));
+    _ = curses.mvadd_wch(y, x + iwidth - 1, &(try cchar_tOf(drawing.tr_corner)));
+    _ = curses.mvadd_wch(y + iheight - 1, x, &(try cchar_tOf(drawing.bl_corner)));
+    _ = curses.mvadd_wch(y + iheight - 1, x + iwidth - 1, &(try cchar_tOf(drawing.br_corner)));
+}
+
+const GameState = struct {
+    x: i32 = 0,
+    y: i32 = 0,
+};
+
+pub fn main() !u8 {
+    // Need for unicode support
+    _ = locale.setlocale(locale.LC_ALL, "");
+    const main_window = curses.initscr().?;
+    defer _ = curses.endwin();
+    // Raw mode to instantly process characters
+    _ = curses.raw();
+    // Activate arrow key support
+    _ = curses.keypad(main_window, true);
+
+    var state = GameState{};
+
+    // Main loop
+    while (true) {
+        switch (curses.getch()) {
+            'q' => return 0,
+            curses.KEY_UP => {state.y -= 1;},
+            curses.KEY_DOWN => {state.y += 1;},
+            curses.KEY_LEFT => {state.x -= 1;},
+            curses.KEY_RIGHT => {state.x += 1;},
+            else => |val| {std.debug.print("Pressed key: {x}\n", .{val});},
+        }
+        _ = curses.erase();
+        try drawBox(state.x, state.y, 15, 10);
+        _ = curses.refresh();
+    }
+    return 0;
 }
